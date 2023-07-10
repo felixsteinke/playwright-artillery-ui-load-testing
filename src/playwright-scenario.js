13d60bf20849d1a4f1506fb5a754c43901dcc487
Felix <48789911+felixsteinke@users.noreply.github.com>
2023-07-10 17:45:49 +0200

    add codegen doc (#1)

--- src/playwright-scenario.js
+++ src/playwright-scenario.js
@@ -4,6 +4,9 @@ async function usageScenario(page) {
     // insert generated code here
 
 
+    // insert generated code here
+}
 
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
